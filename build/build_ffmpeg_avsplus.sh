2dcee82d87d3649cc5dd39dfa802de43c0b4a228
ColderCoder <44255287+ColderCoder@users.noreply.github.com>
2026-08-25 11:36:18 +0800

    Use FFmpeg-AVSPlus source in build script

--- build/build_ffmpeg_avsplus.sh
+++ build/build_ffmpeg_avsplus.sh
@@ -3,26 +3,16 @@ set -e
 
 PREFIX="$PWD/out"
 SRC="$PWD/src"
-FFMPEG_TAG=${FFMPEG_TAG:-n7.1.1}
 
 mkdir -p "$SRC" "$PREFIX"
 
-cd "$SRC"
-
-if [ ! -d ffmpeg ]; then
-  git clone --depth 1 --branch "$FFMPEG_TAG" https://github.com/FFmpeg/FFmpeg.git ffmpeg
-fi
-
-cd ffmpeg
-
-# AVS+ patch stage
-# Apply ffmpeg_cavs_dra/libcavs patch series here.
-# The patch must extend cavs decoder with AVS1-P16 support.
+cd "$SRC/FFmpeg-AVSPlus"
 
 ./configure \
   --prefix="$PREFIX" \
   --enable-gpl \
   --enable-shared \
+  --disable-static \
   --disable-programs \
   --disable-doc
 
